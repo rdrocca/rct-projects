bbae4f40d544b480981c6bbb52ad21ad12cbb8e5
rdrocca <rdrocca.dev@gmail.com>
2021-09-10 18:31:06 -0500

    05 finished

--- 05-menu/src/App.js
+++ 05-menu/src/App.js
@@ -3,10 +3,22 @@ import Menu from './Menu';
 import Categories from './Categories';
 import items from './data';
 
+const allCategories = ['all', ...new Set(items.map((item) => item.category))];
+
 function App() {
 
   const [menuItems, setMenuItems] = useState(items);
-  const [categories, setCategories] = useState([]);
+  // eslint-disable-next-line 
+  const [categories, setCategories] = useState(allCategories);
+
+  const filterItems = (category) => {
+    if (category === 'all') {
+      setMenuItems(items);
+      return;
+    };
+    const newItems = items.filter((item) => item.category === category);
+    setMenuItems(newItems);
+  };
 
   return (
     <main>
@@ -15,8 +27,8 @@ function App() {
           <h2>our menu</h2>
           <div className="underline"></div>
         </div>
-        <Categories></Categories>
-        <Menu items={menuItems}></Menu>
+        <Categories categories={categories} filterItems={filterItems} />
+        <Menu items={menuItems} />
       </section>
     </main>
   );
